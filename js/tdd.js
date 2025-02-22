26819bc26d97da2c3636626117e541466cf42d6b
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-21 20:59:50 -0700

    implement last function but it has some bugs and tests are missing might implement later

--- js/tdd.js
+++ js/tdd.js
@@ -24,8 +24,6 @@ const calculator = {
   },
 };
 
-function caesarCypher(string, offset) {}
-
 function analyzeArray(array) {
   return {
     average: array.reduce((acc, curr) => acc + curr, 0) / array.length,
@@ -35,4 +33,56 @@ function analyzeArray(array) {
   };
 }
 
-module.exports = { capitalize, reverseString, calculator, analyzeArray };
+function isUpper(char) {
+  return char === char.toUpperCase() && char !== char.toLowerCase();
+}
+
+function find(element, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (element === arr[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getNewAlphabet(offset) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let newAlphabet = "";
+  let aux = 0;
+  for (let i = 0; i < alphabet.length; i++) {
+    if (i + offset < alphabet.length) {
+      newAlphabet += alphabet[i + offset];
+    } else {
+      newAlphabet += alphabet[aux];
+      aux++;
+    }
+  }
+  return offset > alphabet.length - 1 ? null : newAlphabet;
+}
+
+function caesarCipher(string, offset) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let newAlphabet = getNewAlphabet(offset);
+
+  if (newAlphabet === null) return null;
+
+  let cipherText = "";
+  for (let i = 0; i < string.length; i++) {
+    const charPos = [...alphabet].indexOf(string[i]);
+    if (!find(string[i], newAlphabet)) {
+      cipherText += string[i];
+    } else {
+      cipherText += newAlphabet[charPos];
+    }
+  }
+  return cipherText;
+}
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  analyzeArray,
+  caesarCipher,
+};
