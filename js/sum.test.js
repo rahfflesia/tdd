fe7dba4e813898aa9f699dc119229e3133a61017
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-24 15:06:45 -0700

    finish tdd

--- js/sum.test.js
+++ js/sum.test.js
@@ -76,7 +76,25 @@ test("Divide", () => {
 });
 
 // Analyze array function tests
-test("Test 1 analyze array", () => {});
+test("Test 1 analyze array", () => {
+  const obj = { average: 1, min: 1, max: 1, length: 5 };
+  expect(mod.analyzeArray([1, 1, 1, 1, 1])).toEqual(obj);
+});
+
+test("Test 2 analyze array", () => {
+  const obj = { average: 8.5, min: 5, max: 12, length: 6 };
+  expect(mod.analyzeArray([5, 6, 10, 11, 12, 7])).toEqual(obj);
+});
+
+test("Test 3 analyze array", () => {
+  const obj = { average: 6.333333333333333, min: -1, max: 10, length: 3 };
+  expect(mod.analyzeArray([10, -1, 10])).toEqual(obj);
+});
+
+test("Test 4 analyze array", () => {
+  const obj = { average: 7.5, min: 5, max: 10, length: 2 };
+  expect(mod.analyzeArray([5, 10])).toEqual(obj);
+});
 
 // Caesar cipher tests
 test("Test 1", () => {
